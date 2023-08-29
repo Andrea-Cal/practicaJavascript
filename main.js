979fcf6b80f35979e66793ff518975bde2132ae9
Andrea-Cal <andiesc83@hotmail.com>
2023-08-29 17:27:26 -0300

    agrego práctica de Funciones de Mindhub

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ console.log(frutas[4]);
 en una variable llamada soyMayorDeEdad y mostrar por consola un mensaje que diga:
 Soy mayor de edad y el valor de la variable. */
 let n = 18;
-let miEdad = prompt("Ingrese su edad");
+let miEdad = +prompt("Ingrese su edad");
 let soyMayorDeEdad = miEdad >= n;
 console.log("Soy mayor de edad: " + soyMayorDeEdad);
 
@@ -354,3 +354,107 @@ for (let i = 0; i < arrayDeNumeros3.length; i++) {
   } 
 }
 console.log(`El numero mas grande es: ${numeroMasGrande}`);
+
+// ----------------------- PRACTICA FUNCIONES -----------------------
+// EJERCICIO 1
+/* Crea una función llamada "saludar" que tome un parámetro de tipo cadena/string que represente un nombre, y muestre en la consola un saludo personalizado utilizando el nombre proporcionado. Llama a la función con tu propio nombre para verificar que funcione correctamente. */
+
+function saludar(nombre){
+  console.log(`Hola ${nombre}! Como estas?`);
+}
+saludar("Andrea");
+
+// EJERCICIO 2
+/* Crea una función llamada "multiplicar" que reciba dos parámetros numéricos y
+devuelva el resultado de la multiplicación de ambos. Llama a la función con dos valores diferentes y muestra el resultado en la consola. */
+
+function multiplicar(a, b){
+  return a*b;
+}
+console.log(multiplicar(3,2));
+console.log(multiplicar(13,14));
+
+// EJERCICIO 3
+/* Crea una función llamada "areaTriangulo" que reciba dos parámetros numéricos que representen la base y la altura de un triángulo, y devuelva el área del mismo. Luego, crea otra función llamada "perimetroTriangulo" que reciba tres parámetros numéricos que representen los lados de un triángulo, y devuelva el perímetro del mismo. Utiliza estas funciones para calcular el área y el perímetro de un triángulo con base 5 y altura 3 y lados de 4, 5 y 6 respectivamente. */
+
+function areaTriangulo(base, altura){
+  return base*altura;
+}
+function perimetroTriangulo(lado1, lado2, lado3){
+  return lado1+lado2+lado3;
+}
+console.log("El area del triangulo es " + areaTriangulo(5, 3));
+console.log("El perimetro del triangulo es " + perimetroTriangulo(4, 5, 6));
+
+// EJERCICIO 4
+/* Crea una función llamada "calcularPrecio" que reciba dos parámetros numéricos que representen el precio y la cantidad de un producto, y devuelva el precio total de la compra. Si la cantidad de productos es mayor o igual a 10, aplica un descuento del 10% al precio total. Si la cantidad de productos es mayor o igual a 20, aplica un descuento del 20% al precio total. Llama a la función con diferentes valores de precio y cantidad para verificar que funcione correctamente */
+
+function calcularPrecio(precio, cantidad){
+  let precioTotal = precio*cantidad;
+  if(cantidad>=20){
+    precioTotal = precioTotal*0.8;
+  }else if(cantidad>=10){
+    precioTotal = precioTotal*0.9;
+  }
+  return precioTotal;  
+}
+console.log("El precio total es: " + calcularPrecio(2, 2));
+console.log("El precio total es: " + calcularPrecio(2, 11));
+console.log("El precio total es: " + calcularPrecio(2, 22));
+
+// EJERCICIO 5
+/* Crea una función llamada "esMayorDeEdad" que reciba un parámetro numérico que represente la edad de una persona, y devuelva un mensaje que indique si la persona es mayor de edad o no. Si la edad es mayor o igual a 18, el mensaje debe decir "Eres mayor de edad". Si la edad es menor a 18, el mensaje debe decir "Eres menor de edad".
+Utiliza el operador ternario para implementar esta función. Llama a la función con diferentes valores de edad para verificar que funcione correctamente. */
+
+function esMayorDeEdad(edad){
+  return edad>=18 ? "Eres mayor de edad" : "Eres menor de edad";  
+}
+console.log(esMayorDeEdad(20));
+console.log(esMayorDeEdad(8));
+
+// EJERCICIO 6
+/* Crea una función llamada "calcularImpuesto" que tome un parámetro numérico que represente el ingreso anual de una persona, y devuelva el impuesto que debe pagar. Si el ingreso es menor o igual a $10,000, el impuesto es del 10% del ingreso. Si el ingreso es mayor a $10,000 pero menor o igual a $20,000, el impuesto es del 15% del ingreso.
+Si el ingreso es mayor a $20,000, el impuesto es del 20% del ingreso. Utiliza
+condicionales anidados para implementar esta función. Llama a la función con
+diferentes valores de ingreso para verificar que funcione correctamente. */
+
+function calcularImpuesto(ingresoAnual){
+  let impuesto = 0;
+  if(ingresoAnual > 20000){
+    impuesto = ingresoAnual*0.20;
+  }else if(ingresoAnual > 10000){
+    impuesto = ingresoAnual*0.15;
+  }else{
+    impuesto = ingresoAnual*0.10;
+  }
+  return impuesto;
+}
+console.log("El impuesto a pagar es de $ "+ calcularImpuesto(9000));
+console.log("El impuesto a pagar es de $ "+ calcularImpuesto(12000));
+console.log("El impuesto a pagar es de $ "+ calcularImpuesto(24000));
+
+// EJERCICIO 7
+/* Crea una función llamada "verificarDia" que reciba un parámetro numérico que
+represente el número del día de la semana, y devuelva un mensaje que indique si es un día laboral o no. Si el número es 1, 2, 3, 4, 5, el mensaje debe decir "Es un día laboral". Si el número es 6 o 7, el mensaje debe decir "Es fin de semana". Utiliza la estructura de control switch anidada para implementar esta función. Llama a la función con diferentes valores de día para verificar que funcione correctamente. */
+
+function verificarDia(dia){
+  switch (dia) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      console.log("Es un día laboral")      
+      break;
+    case 6:
+    case 7:
+      console.log("Es fin de semana")  
+      break;  
+    default: console.log("Ingrese un dia del 1 al 7")
+      break;
+  }
+}
+verificarDia(1);
+verificarDia(5);
+verificarDia(6);
+verificarDia(11);
